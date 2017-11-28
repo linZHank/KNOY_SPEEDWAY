7531541e30cd14e9b5333fd3834e9e6cb6855a13
MHZ <zhan2838@purdue.edu>
2017-11-28 16:43:54 -0500

    Start IMU compensated waypoint following

--- map_follower/src/serial_imu.py
+++ map_follower/src/serial_imu.py
@@ -11,15 +11,14 @@ console_ser = serial.Serial(port = "/dev/ttyACM0", baudrate=115200)
 console_ser.close()
 console_ser.open()
 #construct and send the Ackermann steering commands to rally car 
-console_ser.read(20)
-ser_str = "IMU1"
-console_ser.write(ser_str)
+console_ser.read()
+console_ser.write("IMU1")
 while True:    # time.sleep(0.1)
     if console_ser.inWaiting()>0:
         read = console_ser.read(20)
         #time.sleep(0.2)
         print(read)
-    #time.sleep(0.1)
+    time.sleep(1)
 # console_ser.close()
 
 
