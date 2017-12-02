bd71e056806e9791be5d05c35242dcd2133a9abb
MHZ <zhan2838@purdue.edu>
2017-12-02 16:05:15 -0500

    modify test_imuread.py

--- map_follower/src/test_imuread.py
+++ map_follower/src/test_imuread.py
@@ -6,7 +6,8 @@ import serial
 console_ser = serial.Serial(port = "/dev/ttyACM0", baudrate=115200)
 console_ser.close()
 console_ser.open()
-console_ser.read(60)
+console_ser.read(32)
+console_ser.write('IMU0')
 console_ser.write('IMU1')
 
 def imu_talker():
@@ -15,14 +16,10 @@ def imu_talker():
     rate = rospy.Rate(150) # 10hz
     while not rospy.is_shutdown():
         if console_ser.inWaiting()>0:
-            read = console_ser.read(36)
+            read = console_ser.read(19)
 	    print("IMU read", read)
-	    if 'I' in read and 'U' in read:
-		s = read.find('I')
-		e = read.find('U')
-		car_ser = read[s:e+1]
-    	rospy.loginfo(car_ser)
-    	imupub.publish(car_ser)
+    	rospy.loginfo(read)
+    	imupub.publish(read)
     	rate.sleep()
 
 if __name__ == '__main__':
