ccddaa1a5fc1890b3501e3977e37ea4e3d21b8ca
MHZ <zhan2838@purdue.edu>
2017-12-08 03:30:08 -0500

    write a serial command test

---
+++ knoy_speedway/README.md
@@ -0,0 +1,16 @@
+# Waypoint following for KNOY\_SPEEDWAY 
+
+## IMU estimate car pose
+update car pose 
+> refer to: http://planning.cs.uiuc.edu/node658.html
+IMU was calibrated using 100 reading at stationary 
+## Car control
+1. Determine which point is next
+2. Compute errors
+3. Compute PID control based on errors
+4. Update car states
+## PID control
+* Seems tuning PID parameters is the key to well following waypoints defined path
+* To slow down the car before it entering a turn, more closely arranged waypoints may be needed. 
+
+> Update\_12072017: The lowest value of serial command is 0156 fopr speed control
---
+++ knoy_speedway/src/test_serialcommand.py
@@ -0,0 +1,25 @@
+from __future__ import print_function
+import rospy
+import serial
+
+def clean_shutdown():
+    print("Stopping sending the command...")
+    console_ser.write('A+0000+0000')
+    console_ser.close()
+    return True
+
+console_ser = serial.Serial(port = '/dev/ttyACM0', baudrate=115200)
+console_ser.close()
+console_ser.open()
+
+def main():
+    rospy.init_node('test_serialcommand')
+    for i in range(4000):
+        console_ser.write('A+0000+0156')
+        print("iter: ", i)
+    console_ser.write('A+0000+0000')
+    rospy.on_shutdown(clean_shutdown)
+    #rospy.spin()
+
+if __name__=='__main__':
+    main()
